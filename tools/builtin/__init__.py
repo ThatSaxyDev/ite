cc0eed8f2a4c78875605b0a99e0454f53a267780
Dedeke David Iteoluwakiisi <thatsaxydev@gmail.com>
2026-02-14 05:36:38 +0100

    feat: Add a new grep tool and update the default model in config.toml to minimax-m2.5.

--- tools/builtin/__init__.py
+++ tools/builtin/__init__.py
@@ -1,3 +1,4 @@
+from tools.builtin.grep import GrepTool
 from tools.builtin.list_dir import ListDirTool
 from tools.builtin.edit_file import EditTool
 from tools.builtin.write_file import WriteFileTool
@@ -10,6 +11,7 @@ __all__ = [
     "EditTool",
     "ShellTool",
     "ListDirTool",
+    "GrepTool",
 ]
 
 
@@ -20,4 +22,5 @@ def get_all_builtin_tools() -> list[type]:
         EditTool,
         ShellTool,
         ListDirTool,
+        GrepTool,
     ]
