3bcd3fa257924512bb18df3352d8e31a814e51fb
Dedeke David Iteoluwakiisi <thatsaxydev@gmail.com>
2026-02-14 02:17:50 +0100

    feat: Add `ListDirTool` to the list of builtin tools.

--- tools/builtin/__init__.py
+++ tools/builtin/__init__.py
@@ -1,3 +1,4 @@
+from tools.builtin.list_dir import ListDirTool
 from tools.builtin.edit_file import EditTool
 from tools.builtin.write_file import WriteFileTool
 from tools.builtin.read_file import ReadFileTool
@@ -8,6 +9,7 @@ __all__ = [
     "WriteFileTool",
     "EditTool",
     "ShellTool",
+    "ListDirTool",
 ]
 
 
@@ -17,4 +19,5 @@ def get_all_builtin_tools() -> list[type]:
         WriteFileTool,
         EditTool,
         ShellTool,
+        ListDirTool,
     ]
